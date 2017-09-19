3ec977b062bfc7f9d66c071121b2f43b784615d8
victor <hernanvi@sheridancollege.ca>
2017-09-19 11:22:57 -0400

    starting Android app
    Added Base app

---
+++ AndroidApplication/app/src/main/java/quadcore/androidapplication/SignInLogOn.java
@@ -0,0 +1,13 @@
+package quadcore.androidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInLogOn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_log_on);
+    }
+}
